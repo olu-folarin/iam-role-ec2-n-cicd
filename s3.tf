e1003c2fcc000c8a4cd12d539607a233c8a93126
folarin <folami25@gmail.com>
2023-01-28 04:19:09 +0100

    fix: "midified buclet name"

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "tf_state_bucket" {
-  bucket = "my-tf-deploy-state-bucket"
+  bucket = "my-tffola-deploy-state-bucket"
 }
 
 terraform {
   backend "s3" {
-    bucket = "my-tf-deploy-state-bucket"
+    bucket = "my-tffola-deploy-state-bucket"
     key    = "terraform.tfstate"
     region = "eu-west-2"
   }
